9f8ad7b629c30cff6697f353633e160bac33fe03
Robin Ghyselinck <robin.ghyselinck2@kbc.be>
2022-01-27 16:53:51 +0100

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, {useState} from 'react';
 
 import './App.css';
 
+const Title = () => <h1>give feedback</h1>;
+
 // Conditional rendering depending on the state of the app
 const History = (props) => {
     if (props.allClicks.length === 0) {
@@ -23,27 +25,45 @@ const Button = ({ handleClick, text }) => (
 )
 
 const App = () => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
   const [allClicks, setAll] = useState([]);
 
-  const handleLeftClick = () => {
+    const handleBadClick = () => {
       // Concat returns a new copy of the array with the item added to it
-    setAll(allClicks.concat('L'));
-    setLeft(left + 1);
-  };
+    setAll(allClicks.concat('B'));
+    setBad(bad + 1);
+    };
 
-    const handleRightClick = () => {
-        setAll(allClicks.concat('R'));
-        setRight(right + 1);
+    const handleGoodClick = () => {
+        setAll(allClicks.concat('G'));
+        setGood(good + 1);
+    };
+
+    const handleNeutralClick = () => {
+        // Concat returns a new copy of the array with the item added to it
+        setAll(allClicks.concat('N'));
+        setNeutral(neutral + 1);
     };
 
   return (
       <div>
-        {left}
-            <Button handleClick={handleLeftClick} text='left' />
-            <Button handleClick={handleRightClick} text='right' />
-        {right}
+          <Title />
+          <div className="row">
+              <Button handleClick={handleGoodClick} text='good' />
+              <Button handleClick={handleNeutralClick} text='neutral' />
+              <Button handleClick={handleBadClick} text='bad' />
+          </div>
+          <div>
+              good {good}
+          </div>
+          <div>
+              neutral {neutral}
+          </div>
+          <div>
+              bad {bad}
+          </div>
           <History allClicks={allClicks} />
       </div>
   )
